95c22d1e2f9354ec5166634658a9eac37b8deb94
Mark McBride <mark@turbinelabs.io>
2017-09-10 21:13:34 -0700

    adding linter/prettier

--- on-puck.js
+++ on-puck.js
@@ -1,14 +1,18 @@
+/** @prettier */
+/** eslint semi: "error" */
 // run this on the puck itself
 
-var releasePct = 0;
-var releasing = false;
-var releaseStep = 0;
-var releaseReady = false;
-var redLight = 0;
+/* globals LED1 LED2 LED3 NRF BTN setWatch */
 
+let releasePct = 0;
+let releaseStep = 0;
+let releaseReady = false;
+let redLight = 0;
 
 /**
- *  blink the red LED starting at a time 500ms*i in the future
+ * blink the red LED starting at a time 500ms*i in the future
+ * @param {number} i the delay to blink the light, in half seconds
+ * @returns {undefined}
  */
 function blinks(i) {
   setTimeout(() => {
@@ -16,11 +20,13 @@ function blinks(i) {
   }, i * 500);
   setTimeout(() => {
     leds(0, 1, 0);
-  }, (i * 500) + 250);
+  }, i * 500 + 250);
 }
 
 /**
  * light all LEDs for half a second starting at a time 500ms*i in the future
+ * @param {number} i the delay to start the hold, in half seconds
+ * @returns {undefined}
  */
 function hold(i) {
   setTimeout(() => {
@@ -28,11 +34,12 @@ function hold(i) {
   }, i * 500);
   setTimeout(() => {
     leds(0, 0, 0);
-  }, (i * 500) + 2000);
+  }, i * 500 + 2000);
 }
 
 /**
  * played at the end of the release. Blink red -> green -> blue, then hold
+ * @returns {undefined}
  */
 function wrapUpAnimation() {
   blinks(1);
@@ -43,11 +50,12 @@ function wrapUpAnimation() {
 
 /**
  * toggle blue/green LEDs in proportion to current release weight
+ * @returns {undefined}
  */
 function releaseAnimation() {
   if (releasePct > 0) {
-    releaseStep += 1;
-    if (releaseStep % 10 > releasePct/10) {
+    releaseStep = releaseStep + 1;
+    if (releaseStep % 10 > releasePct / 10) {
       leds(0, 1, 0);
     } else {
       leds(0, 0, 1);
@@ -60,6 +68,7 @@ function releaseAnimation() {
 
 /**
  * pulse the red light if a release is ready
+ * @returns {undefined}
  */
 function pulseRed() {
   if (releaseReady) {
@@ -71,6 +80,13 @@ function pulseRed() {
   }
 }
 
+/**
+ * set all three LEDs to the given values
+ * @param {number} red - turn red on if 1, off if 0
+ * @param {number} green - turn green on if 1, off if 0
+ * @param {number} blue - turn blue on if 1, off if 0
+ * @returns {undefined}
+ */
 function leds(red, green, blue) {
   LED1.write(red);
   LED2.write(green);
@@ -79,14 +95,16 @@ function leds(red, green, blue) {
 
 /**
  * called when the release pct attribute is written
+ * @param {Object} evt the event dispatched by the NRF framework
+ * @returns {undefined}
  */
 function writeReleasePct(evt) {
-  console.log("got event: " + JSON.stringify(evt));
+  console.log('got event: ' + JSON.stringify(evt));
   releasePct = evt.data[0];
-  console.log("release pct: " + releasePct);
+  console.log('release pct: ' + releasePct);
   // if we're all done releasing play the wrapup animation and
   // reset release pct to zero
-  if (releasePct == 100) {
+  if (releasePct === 100) {
     wrapUpAnimation();
     releasePct = 0;
   } else if (releasePct > 0) {
@@ -98,10 +116,12 @@ function writeReleasePct(evt) {
 
 /**
  * called when the release ready attribute is written
+ * @param {Object} evt the event sent by the NRF framework
+ * @returns {undefined}
  */
 function writeReleaseReady(evt) {
-  console.log("release ready: " + JSON.stringify(evt));
-  var ready = parseInt(evt.data.toString());
+  console.log('release ready: ' + JSON.stringify(evt));
+  let ready = parseInt(evt.data.toString());
   if (ready === 0) {
     releaseReady = false;
   } else {
@@ -113,70 +133,74 @@ function writeReleaseReady(evt) {
 /**
  * when the host disconnects quiesce the things
  */
-NRF.on('disconnect', (reason) => {
+NRF.on('disconnect', reason => {
   leds(0, 0, 0);
   releasePct = 0;
-  releasing = false;
-  releaseReady = false;  
+  releaseReady = false;
 });
 
 /**
  * create our BLE attributes and advertise them
  */
-NRF.setServices({
-  0xBCDE: {
-    0xABCD: {
-      description: "Release Ready", 
-      value: "false",
-      writable: true,
-      onWrite: writeReleaseReady
+NRF.setServices(
+  {
+    0xbcde: {
+      0xabcd: {
+        description: 'Release Ready',
+        value: 'false',
+        writable: true,
+        onWrite: writeReleaseReady,
+      },
+      0xabce: {
+        description: 'Desired Release Pct',
+        value: releasePct,
+        notify: true,
+        readable: true,
+      },
+      0xabcf: {
+        description: 'Release Pct',
+        value: '0',
+        writable: true,
+        onWrite: writeReleasePct,
+      },
     },
-    0xABCE: {
-      description: "Desired Release Pct",
-      value: releasePct,
-      notify: true,
-      readable: true
-    },
-    0xABCF: {
-      description: "Release Pct",
-      value: "0",
-      writable: true,
-      onWrite: writeReleasePct
-    }
-  }
-}, {advertise: ['BCDE']});
+  },
+  { advertise: ['BCDE'] },
+);
 
 /**
  * called when the button is pressed
+ * @returns {undefined}
  */
 function btnPressed() {
-  console.log("button pressed");
+  console.log('button pressed');
   // set LEDs off, which may get overwritten by an animation
   leds(0, 0, 0);
   releaseReady = false;
-  var desiredReleasePct = releasePct + 20;
+  let desiredReleasePct = releasePct + 20;
   // clamp
   if (desiredReleasePct > 100) {
     desiredReleasePct = 100;
   }
   // single byte silliness
-  var transmitReleasePct = (desiredReleasePct / 10).toString();
-  if (transmitReleasePct == "10") {
-    transmitReleasePct = "A";
+  let transmitReleasePct = (desiredReleasePct / 10).toString();
+  if (transmitReleasePct === '10') {
+    transmitReleasePct = 'A';
   }
-  console.log("sending drp " + transmitReleasePct);
+  console.log('sending drp ' + transmitReleasePct);
   NRF.updateServices({
-    0xABCD: {
-       0xABCE: {
-         value: transmitReleasePct,
-         notify: true
-       }
-    }
+    0xabcd: {
+      0xabce: {
+        value: transmitReleasePct,
+        notify: true,
+      },
+    },
   });
 }
 
 // set up a handler for our button press
-setWatch(btnPressed, BTN, {edge:"rising",
-                           repeat:true,
-                           debounce:50});
-
+setWatch(btnPressed, BTN, {
+  edge: 'rising',
+  repeat: true,
+  debounce: 50,
+});
